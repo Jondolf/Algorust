a3fa384d88d0edd6e746d215e321bf137593dff7
Jondolf <joona.m.aalto@outlook.com>
2022-03-12 13:50:31 +0200

    Make sidebar responsive, improve styling
    The sidebar now moves to he bottom when screen width is under 600px.
    The sidebar can be resized horizontally on large screens,
    and vertically on small screens. This should work on mobile as well.
    
    Also changed some styles a bit and added a use_mouse_pos hook.

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -1,9 +1,13 @@
-use log::info;
 use wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::window;
 use yew::prelude::*;
+use yew_hooks::use_window_size;
 
 use crate::hooks::use_mouse_pos::use_mouse_pos;
+
+/// The screen width where the layout changes and the sidebar moves to the bottom
+const MOBILE_MODE_THRESHOLD: i32 = 600;
+const RESIZE_HANDLE_THICKNESS_PX: i32 = 4;
 
 #[derive(Properties, Clone, PartialEq)]
 pub struct SidebarProps {
@@ -13,37 +17,70 @@ pub struct SidebarProps {
 
 #[function_component(Sidebar)]
 pub fn sidebar(props: &SidebarProps) -> Html {
-    let width_px = use_state_eq(|| 350);
-    let resizing = use_state_eq(|| false);
-    let (mouse_x, _) = use_mouse_pos();
+    let width_px = use_state_eq(|| 350); // Used on large screens, sidebar on the left side
+    let height_px = use_state_eq(|| 350); // Used on small screens, sidebar at the bottom
+    let resizing_width = use_state_eq(|| false);
+    let resizing_height = use_state_eq(|| false);
+    let (window_width, window_height) = use_window_size();
+    let (mouse_x, mouse_y) = use_mouse_pos();
 
-    let on_handle_pressed = {
-        let resizing = resizing.clone();
+    let on_resize_width_mousedown = {
+        let resizing_width = resizing_width.clone();
 
         Callback::from(move |e: MouseEvent| {
-            info!("Hello {}", *resizing);
             e.prevent_default();
             e.stop_propagation();
-            resizing.set(true);
+            resizing_width.set(true);
+        })
+    };
+    let on_resize_width_touchstart = {
+        let resizing_width = resizing_width.clone();
+
+        Callback::from(move |e: TouchEvent| {
+            e.prevent_default();
+            e.stop_propagation();
+            resizing_width.set(true);
+        })
+    };
+
+    let on_resize_height_mousedown = {
+        let resizing_height = resizing_height.clone();
+
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            e.stop_propagation();
+            resizing_height.set(true);
+        })
+    };
+    let on_resize_height_touchstart = {
+        let resizing_height = resizing_height.clone();
+
+        Callback::from(move |e: TouchEvent| {
+            e.stop_propagation();
+            resizing_height.set(true);
         })
     };
 
     {
-        let resizing = resizing.clone();
+        let resizing_width = resizing_width.clone();
+        let resizing_height = resizing_height.clone();
 
         use_effect_with_deps(
             move |_| {
-                let mouseup_listener =
-                    Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |_| resizing.set(false)));
+                let callback = Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |_| {
+                    resizing_width.set(false);
+                    resizing_height.set(false);
+                }));
                 window()
                     .unwrap()
-                    .add_event_listener_with_callback(
-                        "mouseup",
-                        mouseup_listener.as_ref().unchecked_ref(),
-                    )
+                    .add_event_listener_with_callback("mouseup", callback.as_ref().unchecked_ref())
+                    .unwrap();
+                window()
+                    .unwrap()
+                    .add_event_listener_with_callback("touchend", callback.as_ref().unchecked_ref())
                     .unwrap();
 
-                move || drop(mouseup_listener)
+                move || drop(callback)
             },
             (),
         );
@@ -51,29 +88,55 @@ pub fn sidebar(props: &SidebarProps) -> Html {
 
     {
         let width_px = width_px.clone();
-        let resizing = resizing.clone();
+        let height_px = height_px.clone();
+        let resizing_width = resizing_width.clone();
+        let resizing_height = resizing_height.clone();
 
         use_effect_with_deps(
-            move |x| {
-                let window_width = window().unwrap().inner_width().unwrap().as_f64().unwrap();
-                // Minimum width is 1 to ensure that the resize handle is always available
-                if *resizing && *x >= 1 && *x <= (window_width * 0.75) as i32 {
-                    width_px.set(*x);
+            move |(x, y, win_width, win_height)| {
+                let (new_width, new_height) = (*x, *win_height as i32 - *y);
+                let (max_width, max_height) =
+                    ((win_width * 0.75) as i32, (win_height * 0.75) as i32);
+                let min = RESIZE_HANDLE_THICKNESS_PX;
+
+                if *width_px > max_width && *win_width >= MOBILE_MODE_THRESHOLD as f64 {
+                    width_px.set(max_width);
+                }
+                if *height_px > max_height {
+                    height_px.set(max_height);
+                }
+
+                if *resizing_width && new_width >= min && new_width <= max_width {
+                    width_px.set(new_width);
+                }
+
+                if *resizing_height && new_height >= min && new_height <= max_height {
+                    height_px.set(new_height);
                 }
                 || ()
             },
-            mouse_x,
+            (mouse_x, mouse_y, window_width, window_height),
         );
     }
 
     html! {
-        <div class="sidebar" style={format!("min-width: {}px; max-width: {}px", *width_px, *width_px)}>
+        <div class="sidebar" style={format!("min-width: {}px; min-height: {}px", *width_px, *height_px)}
+        >
             <div
-                class="resize-handle"
-                style={if *resizing { "opacity: 1" } else { "" }}
-                onmousedown={on_handle_pressed}>
+                class="resize-handle resize-width"
+                style={if *resizing_width { "opacity: 1" } else { "" }}
+                onmousedown={on_resize_width_mousedown}
+                ontouchstart={on_resize_width_touchstart}>
             </div>
-            { for props.children.iter() }
+            <div
+                class="resize-handle resize-height"
+                style={if *resizing_height { "opacity: 1" } else { "" }}
+                onmousedown={on_resize_height_mousedown}
+                ontouchstart={on_resize_height_touchstart}>
+            </div>
+            <div class="sidebar-content">
+                { for props.children.iter() }
+            </div>
         </div>
     }
 }
--- src/hooks/use_mouse_pos.rs
+++ src/hooks/use_mouse_pos.rs
@@ -7,21 +7,41 @@ pub fn use_mouse_pos() -> (i32, i32) {
 
     {
         let mouse_pos = mouse_pos.clone();
+        let touch_pos = mouse_pos.clone();
 
         use_effect_with_deps(
             move |_| {
-                let listener =
-                    Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |e: MouseEvent| {
+                let pointermove_cb =
+                    Closure::<dyn Fn(PointerEvent)>::wrap(Box::new(move |e: PointerEvent| {
                         mouse_pos.set((e.page_x(), e.page_y()));
                     }));
+                let touchmove_cb =
+                    Closure::<dyn Fn(TouchEvent)>::wrap(Box::new(move |e: TouchEvent| {
+                        touch_pos.set((
+                            e.touches().item(0).unwrap().page_x(),
+                            e.touches().item(0).unwrap().page_y(),
+                        ));
+                    }));
+
                 window()
                     .unwrap()
                     .add_event_listener_with_callback(
-                        "mousemove",
-                        listener.as_ref().unchecked_ref(),
+                        "pointermove",
+                        pointermove_cb.as_ref().unchecked_ref(),
                     )
                     .unwrap();
-                move || drop(listener)
+                window()
+                    .unwrap()
+                    .add_event_listener_with_callback(
+                        "touchmove",
+                        touchmove_cb.as_ref().unchecked_ref(),
+                    )
+                    .unwrap();
+
+                move || {
+                    drop(pointermove_cb);
+                    drop(touchmove_cb);
+                }
             },
             (),
         );
