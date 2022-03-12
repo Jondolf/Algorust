4f4d6c59c52e1b3a2d72c0f3e9d9e22deae55682
Jondolf <joona.m.aalto@outlook.com>
2022-03-12 16:57:38 +0200

    Add icons and PWA support
    Added icons, manifest.json and a service worker to make the app
    installable as a Progressive Web Application.

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = "algorithms";
+const filesToCache = ["/index.html", "/index-1940e887830c7517_bg.wasm"];
+
+// Start the service worker and cache all of the app's content
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
