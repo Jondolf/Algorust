630db50260e0a2ac0620331cbd2012b20b41d56a
Jondolf <joona.m.aalto@outlook.com>
2022-07-22 20:16:57 +0300

    Add two links in Home header, update About section

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,4 +1,7 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::Route;
 
 #[function_component(HomePage)]
 pub fn home_page() -> Html {
@@ -7,7 +10,25 @@ pub fn home_page() -> Html {
             <header>
                 <h1>{ "Rust Algorithms" }</h1>
 
-                <p>{ "Visualize various types of algorithms in a responsive, interactive and configurable way." }</p>
+                <p>{ "Visualize various types of algorithms in a responsive, interactive and configurable way. Everything is made with Rust 🦀" }</p>
+
+                <div class="buttons">
+                    <Link<Route> to={Route::Sorting}>
+                        <button class="sorting-button">{ "Get sorting" }</button>
+                    </Link<Route>>
+
+                    <a href="https://github.com/Jondolf/rust-algorithms" target="_blank" aria-label="Link to this website's GitHub repository (opens in a new window)">
+                        <button class="github-button">
+                            <img
+                                src="/assets/images/GitHub-Mark-Light-64px.png"
+                                alt="GitHub logo"
+                                width="20"
+                                height="20"
+                            />
+                            { "View the code" }
+                        </button>
+                    </a>
+                </div>
             </header>
 
             <section class="about">
@@ -16,7 +37,14 @@ pub fn home_page() -> Html {
                 <h3>{ "How is this project made?" }</h3>
 
                 <p>
-                    { "Everything in this project is made entirely by me in Rust with the " } <a href="https://yew.rs" target="_blank">{ "Yew web framework" }</a> { ". If you want, you can view the code at " } <a href="https://github.com/Jondolf/rust-algorithms" target="_blank">{ "the project's GitHub repository" }</a> { ". I am not a Rust, Yew or algorithm expert however (I'm in high school), so many things may not necessarily be optimal or compliant with best practises."
+                    {
+                        "Practically everything in this project is made entirely by me in Rust with the " } <a href="https://yew.rs" target="_blank">{ "Yew web framework" }</a> { ". Only the necessities such as config files, the root " } <code>{ "index.html" }</code> { " file and the SCSS styles are not written in Rust, as it's not currently feasibly possible to do so."
+                    }
+                </p>
+
+                <p>
+                    {
+                        "If you want, you can view the code at the project's " } <a href="https://github.com/Jondolf/rust-algorithms" target="_blank">{ "GitHub repository" }</a> { ". I am not a Rust, Yew or algorithm expert however (I'm in high school), so many things may not necessarily be optimal or compliant with best practises."
                     }
                 </p>
 
@@ -44,7 +72,7 @@ pub fn home_page() -> Html {
 
                 <p>
                     {
-                        "I don't really have any concrete plans that I will stick to, as this is a hobby project and I will usually be busy with school when I'm not on vacation."
+                        "I don't really have any concrete plans that I will stick to, as this is a hobby project and I will usually be busy with school outside of vacations."
                     }
                 </p>
 
@@ -66,9 +94,7 @@ pub fn home_page() -> Html {
                             <li>{ "Pathfinding: weight maps" }</li>
                         </ul>
                     </li>
-                    <li>{ "More algorithm descriptions" }</li>
-                    <li>{ "Render algorithm code on the page" }</li>
-                    <li>{ "Website themes" }</li>
+                    <li>{ "Render algorithm code implementations on the page (at least pseudocode and Rust, maybe others as well)" }</li>
                 </ul>
             </section>
         </div>
